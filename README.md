3c0fc42b6ab4e67d2cbd3d213b4dff8aa1f5b190
kaique-vilela <kaiqueputini2006@gmail.com>
2024-02-19 08:38:19 -0300

    Estudo sobre estrutura de condição.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # aulas_automacao_tarefas3infoB
+Este projeto contém os arquivos das aulas de automação de tarefas.
---
+++ aula2/if.py
@@ -0,0 +1,16 @@
+print('Digite sua idade: ')
+idade = int(input())
+
+print('No ano que vem a sua idade será',int(idade) + 1)
+
+if idade <= 12:
+    print('Você é uma criança')
+
+elif idade < 18:
+    print('Você é um adolescente')
+
+elif idade < 60:
+    print('Você é um adulto')
+
+else:
+    print('Você é um idoso')
