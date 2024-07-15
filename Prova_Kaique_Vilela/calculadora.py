1db06502c1dd6548a65a9122d0fdf1d711bc4410
Kaiquevcp <kaiqueputini2006@gmail.com>
2024-07-15 07:41:29 -0300

    prova

---
+++ Prova_Kaique_Vilela/calculadora.py
@@ -0,0 +1,17 @@
+import pyautogui, time
+
+pyautogui.click(780,1050,interval=0.5)
+pyautogui.write('calculadora')
+pyautogui.click(764,450,duration=0.5)
+pyautogui.click(clicks=2)
+pyautogui.click(73,88,interval=0.5)
+pyautogui.click(73,250,interval=0.5)
+pyautogui.click(73,220,interval=0.5)
+pyautogui.click(213,504,interval=0.5)
+pyautogui.click(213,470,interval=0.5,clicks=2)
+pyautogui.click(75,266)
+
+
+
+
+
